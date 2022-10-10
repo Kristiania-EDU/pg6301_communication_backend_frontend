56116d4cd1a9d0920d9c13e3924eff0d387d3b10
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-10 09:39:15 +0200

    Basic ExpressJS Server

--- client/index.jsx
+++ client/index.jsx
@@ -5,3 +5,13 @@ import {createRoot} from "react-dom/client";
 const element = document.getElementById('app');
 const root = createRoot(element);
 
+
+const App = () => {
+  return(
+    <div>
+        <h1>Hello React</h1>
+    </div>
+  );
+};
+
+root.render(<App></App>);
